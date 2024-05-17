981544f353e70234930b3a0a32c2c8a23585378f
Taky0ndon <char@za.rd>
2024-05-17 17:45:25 -0400

    renamed current_image to current_imagetk to more easily tell when it's an image.Image and when its an Imagetk.PhotoImage

--- src/Layout.py
+++ src/Layout.py
@@ -1,12 +1,11 @@
 import tkinter as tk
 from pathlib import Path
-from tkinter import ttk, messagebox
+from tkinter import PhotoImage, ttk, messagebox
 from PIL import Image, ImageTk
 
 
 class Layout:
     def __init__(self, frame):
-        self.image_display_size = (480, 270)
         self.frame = frame
         self.watermark_path_var = tk.StringVar()
         self.image_path_var = tk.StringVar()
@@ -23,33 +22,39 @@ class Layout:
                                                   )
         self.image_display = ttk.Label(frame)
         self.watermark_display_label = ttk.Label(frame)
-        self.current_image = None
         self.image_description_label = ttk.Label(frame,
                                                  text="Image:"
                                                  )
         self.watermark_description_label = ttk.Label(frame,
                                                  text="Watermark:"
                                                  )
-
+        self.current_image = None
+        self.current_imagetk = None
 
     def set_image(self):
         self.image_path_str = self.image_path_var.get()
         self.image_path = Path(self.image_path_str)
-        if not self.path_is_valid(self.image_path):
+        if not path_is_valid(self.image_path):
             messagebox.showerror(self.frame,
                                message=f"The path {self.image_path_str} does not exit"
                                  )
             return
-        self.current_image = Image.open(self.image_path_var.get())\
-            .resize(self.image_display_size)
-        self.current_image = ImageTk.PhotoImage(self.current_image)
+        self.current_image = Image.open(self.image_path_var.get())
+#            .resize(get_img_display_size(self.current_image))
+        orig_width, orig_height = self.current_image.size
+        new_width = orig_width//4
+        new_height = orig_height//4
+        self.current_imagetk = ImageTk.PhotoImage(self.current_image)
+
+
     def display_image(self):
         self.set_image()
-        self.image_display.configure(image=self.current_image)
+        self.image_display.configure(image=self.current_imagetk)
+
 
     def set_watermark(self, watermark_path_var):
         self.watermark_path = Path(watermark_path_var)
-        if not self.path_is_valid(self.watermark_path):
+        if not path_is_valid(self.watermark_path):
             messagebox.showerror(self.frame,
                                message=f"The path {self.watermark_path.as_posix()} does not exit"
                                  )
@@ -57,13 +62,18 @@ class Layout:
         self.watermark_image = Image.open(self.watermark_path).resize(self.image_display_size)
         self.watermark_photoimage = ImageTk.PhotoImage(self.watermark_image)
 
+
     def display_watermark(self):
         self.set_watermark(self.watermark_path_var.get())
         self.watermark_display_label.configure(image=self.watermark_photoimage)
 
 
-    @staticmethod
-    def path_is_valid(path: Path) -> bool:
-        if not path.exists():
-            return False
-        return True
+def path_is_valid(path: Path) -> bool:
+    if not path.exists():
+        return False
+    return True
+
+def get_img_display_size(img: Image.Image) -> tuple[int, int]:
+    original_dim = img.size # (w, h)
+    reduced_dim = original_dim[0]//4, original_dim[1]//4
+    return reduced_dim
