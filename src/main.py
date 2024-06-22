352a8c6f51253c084ad209bd405f3ee62231e9c8
Taky0ndon <char@za.rd>
2024-06-21 22:49:25 -0400

    created main() function which returns a Layout object that control the gui. added if __name__ == "__main__" statement

--- src/main.py
+++ src/main.py
@@ -13,6 +13,8 @@ DEFAULT_FOLDER = PurePath("/home", "mike", "bg")
 WATERMARK_DIR = Path(Path(os.getcwd()).parent, "assets", "img")
 
 
+def main() -> Layout:
+    return Layout()
 
-
-layout = Layout()
+if __name__ == "__main__":
+    main()
