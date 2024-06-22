396504545adbbf04f5d1366138ba3e731ad850f0
Taky0ndon <char@za.rd>
2024-06-21 22:46:38 -0400

    moved layout configuration out of main.py and into Layout.py

--- src/Layout.py
+++ src/Layout.py
@@ -16,40 +16,48 @@ FG_STARTING_DIR = Path(IMG_HOME, "fg")
 img_mgr = ImageManager()
 
 class Layout:
-    def __init__(self, frame: tk.Frame):
-        self.frame = frame
-        self.total_columns, self.total_rows = 0, 0
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Watermark Me")
+        self.mainframe = ttk.Frame(self.root, padding="3 3 12 12")
+        self.mainframe.grid(column=0, row=0, sticky="NWSE")
+        self.create_ui()
+        self.root.mainloop()
+        #self.total_columns, self.total_rows = 0, 0
+
+    def create_ui(self)-> None:
+       # for child in self.mainframe.winfo_children():
+       #     child.grid_configure(padx=5, pady=5)
+
         self.strvar_fg_path = tk.StringVar()
         self.strvar_bg_path = tk.StringVar()
         self.strvar_fg_position = tk.StringVar(value="0,0")
         self.strvar_fg_text = tk.StringVar()
 
-        self.entry_bg_path = ttk.Entry(frame, textvariable=self.strvar_bg_path)
-        self.entry_fg_path = ttk.Entry(frame, textvariable=self.strvar_fg_path)
-        self.entry_fg_position = ttk.Entry(frame, textvariable=self.strvar_fg_position)
-        self.entry_text = ttk.Entry(frame, textvariable=self.strvar_fg_text)
+        self.entry_bg_path = ttk.Entry(self.mainframe, textvariable=self.strvar_bg_path)
+        self.entry_fg_path = ttk.Entry(self.mainframe, textvariable=self.strvar_fg_path)
+        self.entry_fg_position = ttk.Entry(self.mainframe, textvariable=self.strvar_fg_position)
+        self.entry_text = ttk.Entry(self.mainframe, textvariable=self.strvar_fg_text)
 
-        self.btn_select_bg = ttk.Button(frame,
+        breakpoint()
+        self.btn_select_fg = ttk.Button(self.mainframe, text="choose the foreground",
+                                        command=self.display_fg_img)
+        self.btn_select_bg = ttk.Button(self.mainframe,
                                         text="choose background image",
                                         command=self.display_bg_img)
-        self.btn_select_fg = ttk.Button(frame, text="choose the foreground",
-                                        command=self.display_fg_img)
-        self.btn_superimpose = ttk.Button(frame, text="Superimpose image",
+        self.btn_superimpose = ttk.Button(self.mainframe, text="Superimpose image",
                                           command=self.display_superimposed_image)
-        self.btn_drawtxt = ttk.Button(frame, text="Add text",
+        self.btn_drawtxt = ttk.Button(self.mainframe, text="Add text",
                                       command=self.display_superimposed_text)
-        self.btn_save = ttk.Button(frame, text="Save.", command=img_mgr.save_img)
-        self.btn_exit = ttk.Button(frame, text="exit", command=exit)
+        self.btn_save = ttk.Button(self.mainframe, text="Save.", command=img_mgr.save_img)
+        self.btn_exit = ttk.Button(self.mainframe, text="exit", command=exit)
 
-        self.label_bg_indicator = ttk.Label(frame, text="Background:")
-        self.label_fg_indicator = ttk.Label(frame, text="Foreground:")
-        self.label_fg_position = ttk.Label(frame, text="Foreground position (x, y)")
-        self.label_bg_display = ttk.Label(frame)
-        self.label_fg_display = ttk.Label(frame)
-        self.label_finalimg_display = ttk.Label(frame)
-
-    def create_ui(self)-> None:
-
+        self.label_bg_indicator = ttk.Label(self.mainframe, text="Background:")
+        self.label_fg_indicator = ttk.Label(self.mainframe, text="Foreground:")
+        self.label_fg_position = ttk.Label(self.mainframe, text="Foreground position (x, y)")
+        self.label_bg_display = ttk.Label(self.mainframe)
+        self.label_fg_display = ttk.Label(self.mainframe)
+        self.label_finalimg_display = ttk.Label(self.mainframe)
         self.btn_select_bg.grid(column=0, row=0, sticky="WE")
         self.entry_bg_path.grid(column=1, row=0)
         self.btn_select_fg.grid(column=0, row=1, sticky="WE")
@@ -67,11 +75,16 @@ class Layout:
         self.btn_save.grid(column=0, row=8, sticky="WE")
         self.btn_exit.grid(column=1, row=8, sticky="EW")
 
+        # self.get_frame_size()
+        #for child in self.mainframe.winfo_children():
+        #    if isinstance(child, ttk.Entry):
+        #        child.grid(columnspan=self.total_columns, sticky="WE")
+
 
     def choose_bg_file(self) -> None:
         """ Inserts path of file chosen by user into background entry widget
         """
-        bg_img_path = askopenfilename(parent=self.frame,
+        bg_img_path = askopenfilename(parent=self.mainframe,
                               title="Choose background image",
                               initialdir=BG_STARTING_DIR,
                               initialfile=sorted(os.listdir(BG_STARTING_DIR))[0]
@@ -81,7 +94,7 @@ class Layout:
         self.entry_bg_path.xview(len(bg_img_path))
 
     def choose_fg_file(self):
-        fg_img_path = askopenfilename(parent=self.frame,
+        fg_img_path = askopenfilename(parent=self.mainframe,
                               title="Choose foreground image",
                               initialdir=FG_STARTING_DIR,
                               initialfile=sorted(os.listdir(FG_STARTING_DIR))[0]
@@ -121,5 +134,5 @@ class Layout:
 
 
     def get_frame_size(self):
-        if self.frame.grid_size() is not None:
-            self.total_columns, self.total_rows = self.frame.grid_size()
+        if self.mainframe.grid_size() is not None:
+            self.total_columns, self.total_rows = self.mainframe.grid_size()
--- src/main.py
+++ src/main.py
@@ -13,30 +13,6 @@ DEFAULT_FOLDER = PurePath("/home", "mike", "bg")
 WATERMARK_DIR = Path(Path(os.getcwd()).parent, "assets", "img")
 
 
-root = tk.Tk()
-
-root.title("Watermark Me")
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky="NWES")
-
-layout = Layout(mainframe)
-layout.create_ui()
 
 
-for child in mainframe.winfo_children():
-    child.grid_configure(padx=5, pady=5)
-
-# Establish rows and columns before adjusting span
-
-# Adjust spans
-# Could I do this in a loop? 
-layout.get_frame_size()
-for child in mainframe.winfo_children():
-    if isinstance(child, ttk.Entry):
-        child.grid(columnspan=layout.total_columns, sticky="WE")
-layout.btn_superimpose.grid(columnspan=layout.total_columns)
-layout.entry_bg_path.focus()
-
-
-
-root.mainloop()
+layout = Layout()
